c91beee0a1db69735ae1237d728ccbf2c2e66edb
Rafeh Qazi <rafehqazi@Rafehs-MBP.attlocal.net>
2019-08-07 18:24:21 -0700

    intial commit.

---
+++ my_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Search(models.Model):
+    search = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.search)
+
+    class Meta:
+        verbose_name_plural = 'Searches'
