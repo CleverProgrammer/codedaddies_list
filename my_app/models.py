d7e313607ce477dc5d22e3397c0bfbcfc6cf6613
kawalshah <50735149+kawalshah@users.noreply.github.com>
2021-10-11 20:16:10 +0530

    Update models.py

--- my_app/models.py
+++ my_app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Search(models.Model):
     search = models.CharField(max_length=500)
     created = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length =500)
 
     def __str__(self):
         return '{}'.format(self.search)
